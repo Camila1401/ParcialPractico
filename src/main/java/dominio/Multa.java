4e865cdf72afd6e13527925a0f992f83aa0810ff
celeslvp <celestefchallen@gmail.com>
2021-08-21 15:25:02 -0300

    timer para el prestamo

--- src/main/java/dominio/Multa.java
+++ src/main/java/dominio/Multa.java
@@ -2,4 +2,19 @@ package dominio;
 
 public class Multa {
     private int dias;
+
+    Multa(){
+        dias = 0;
+    }
+
+    public void agregarDia(){
+        dias += 5;
+    }
+
+    public void sacarDia() {
+        if (dias > 0) {
+            dias -= 1;
+        }
+    }
+
 }
--- src/main/java/dominio/Prestamo.java
+++ src/main/java/dominio/Prestamo.java
@@ -3,24 +3,48 @@ package dominio;
 import java.util.Date;
 import java.util.List;
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class Prestamo {
     private Date fecha; //dsp vemos si queda date
     private List<Observer> observadores;
     private Usuario usuario;
     private final int dias;
+    private int diasPrestado;
     private Prestable prestable;
     private Multa multa;
     private Timer timer; //dsp tengo q ver como funciona esto
 
-    public Prestamo(Date fechaDePrestamo, List<Observer> observadoresPrestamo, Usuario usuarioPrestamo, Prestable prestablePrestamo){
-        fecha = fechaDePrestamo;
+    public Prestamo(List<Observer> observadoresPrestamo, Usuario usuarioPrestamo, Prestable prestablePrestamo){
+        fecha = new Date();
         observadores = observadoresPrestamo;
         usuario = usuarioPrestamo;
         prestable = prestablePrestamo;
         dias = prestable.cantidadDias();
-        //falta el timer
+        diasPrestado = 0;
+        multa = new Multa();
+        timer = new Timer();
+
+        TimerTask tarea = new TimerTask(){
+
+            @Override
+            public void run() {
+                diasPrestado += 1;
+                if(diasPrestado < dias){
+                    if(dias - diasPrestado == 3){
+                        notificarAvisoAnticipado();
+                    }
+                } else if(diasPrestado == dias){
+                    notificarTiempoAgotado();
+                } else {
+                    multa.agregarDia();
+                }
+            }
+        };
+
+        timer.schedule(tarea, fecha, 86400000); //eso es un dia en milisegundos
     }
+
 
     public void notificarTiempoAgotado(){
         //algo del timer aca o en otro lado q llame a notificar
@@ -30,5 +54,6 @@ public class Prestamo {
     public void notificarAvisoAnticipado(){
         observadores.forEach(observer -> observer.notificarAvisoAnticipado(usuario, prestable));
     }
+
 }
 
